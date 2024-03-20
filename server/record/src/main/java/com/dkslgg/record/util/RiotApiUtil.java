d76bea4c2e030df9944d25dd8707ccc816a51481
유영 <dbdud1999@naver.com>
2024-03-20 20:34:24 +0900

    #3 :sparkles: 닉네임으로 유저 정보 가져오는 기능, 두글자 닉네임 띄어쓰기  삽입 기능 추가

--- server/record/src/main/java/com/dkslgg/record/util/RiotApiUtil.java
+++ server/record/src/main/java/com/dkslgg/record/util/RiotApiUtil.java
@@ -1,8 +1,10 @@
 package com.dkslgg.record.util;
 
 import com.dkslgg.record.model.dto.MatchDto;
+import com.dkslgg.record.model.dto.api.AccountDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,6 +18,25 @@ public class RiotApiUtil {
 
     private RiotApiUtil(@Value("${api.riot.secretkey}") String apiKey) {
         webClient = WebClient.builder().baseUrl("https://asia.api.riotgames.com").defaultHeader("X-Riot-Token", apiKey).build();
+    }
+
+    public String formatRiotId(String riotId) {
+        if(riotId.length() == 2) {
+            riotId = riotId.charAt(0) + " " + riotId.charAt(1);
+        }
+
+        return riotId;
+    }
+
+    public AccountDto requestPuuidByRiotId(String gameName, String tagLine) {
+        return webClient.get().uri("/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}", gameName, tagLine)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    throw new RecordException(ErrorMessage.RIOT_ID_NOT_FOUND);
+                }).onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+                    throw new RecordException(ErrorMessage.RIOT_API_FAILED);
+                })
+                .bodyToMono(AccountDto.class).block();
     }
 
     //@Transactional(rollbackFor = RecordException.class)
