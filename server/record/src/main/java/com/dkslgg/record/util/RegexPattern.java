686748ad251d2b608c023e4480c9a3e23c54b159
유영 <dbdud1999@naver.com>
2024-03-21 00:19:24 +0900

    #3 :wrench: 정규표현식 추가

---
+++ server/record/src/main/java/com/dkslgg/record/util/RegexPattern.java
@@ -0,0 +1,6 @@
+package com.dkslgg.record.util;
+
+public class RegexPattern {
+    public static final String matchId = "^[A-Z]{2}_[0-9]{10}$";
+    public static final String riotId = "^[A-Z0-9가-힣\\s]{2,16}#[A-Z0-9가-힣\\s]{2,5}$";
+}
