a4d955e2b1cfca934c475c70e2f7b99a9000ceb9
l0u0h0 <l0u0h0@gmail.com>
2024-03-30 18:21:35 +0900

    :art: SignupComponent 구조 개선

--- client/dkslgg/src/components/user/SignupComponent.tsx
+++ client/dkslgg/src/components/user/SignupComponent.tsx
@@ -1,5 +1,5 @@
 // React
-import { useMemo, useState, useEffect, useRef } from 'react';
+import React, { useMemo, useState, useEffect, useRef } from 'react';
 // Styled
 import * as S from '@/styles/user/signup.style';
 // Validation
@@ -17,6 +17,133 @@ import {
   SignupFields,
 } from '@/types/component/user.types';
 
+const InputFieldsForm: React.FC<{
+  debouncedValues: SignupFields;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}> = React.memo(
+  ({ debouncedValues, onChange }) => {
+    const [checked, setChecked] = useState<{
+      [key: string]: boolean;
+    }>({
+      name: false,
+      clientId: false,
+      password: false,
+      passwordCheck: false,
+      phone: false,
+      email: false,
+    });
+
+    useEffect(() => {
+      let checks = { ...checked };
+      let result = false;
+
+      Object.keys(debouncedValues).forEach((key) => {
+        const value = debouncedValues[key];
+        switch (key) {
+          case 'name':
+            result = nameValidationCheck(value);
+            break;
+          case 'clientId':
+            result = idValidationCheck(value);
+            break;
+          case 'password':
+            result = pwValidationCheck(value);
+            checks['passwordCheck'] = pwEqualValidationCheck(
+              value,
+              debouncedValues.passwordCheck
+            );
+            break;
+          case 'phone':
+            result = phoneValidationCheck(value);
+            break;
+          case 'email':
+            result = emailValidationCheck(value);
+            break;
+          case 'passwordCheck':
+            result = pwEqualValidationCheck(debouncedValues.password, value);
+            break;
+          default:
+            break;
+        }
+        checks[key] = result;
+      });
+
+      setChecked(checks);
+    }, [debouncedValues]);
+
+    return (
+      <div>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => onChange(e)}
+          placeholder="리그오브레전드 닉네임을 입력하세요."
+          style={{
+            borderBottom: checked.name ? '2px solid green' : '2px solid red',
+          }}
+        />
+        <input
+          type="text"
+          name="clientId"
+          onChange={(e) => onChange(e)}
+          placeholder="아이디를 입력하세요."
+          style={{
+            borderBottom: checked.clientId
+              ? '2px solid green'
+              : '2px solid red',
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => onChange(e)}
+          placeholder="비밀번호를 입력하세요."
+          style={{
+            borderBottom: checked.password
+              ? '2px solid green'
+              : '2px solid red',
+          }}
+        />
+        <input
+          type="password"
+          name="passwordCheck"
+          onChange={(e) => onChange(e)}
+          placeholder="비밀번호를 다시 한번 입력하세요."
+          style={{
+            borderBottom: checked.passwordCheck
+              ? '2px solid green'
+              : '2px solid red',
+          }}
+        />
+        <input
+          type="text"
+          name="phone"
+          onChange={(e) => onChange(e)}
+          placeholder="전화번호를 입력하세요."
+          style={{
+            borderBottom: checked.phone ? '2px solid green' : '2px solid red',
+          }}
+        />
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => onChange(e)}
+          placeholder="이메일을 입력하세요."
+          style={{
+            borderBottom: checked.email ? '2px solid green' : '2px solid red',
+          }}
+        />
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      JSON.stringify(prevProps.debouncedValues) ===
+      JSON.stringify(nextProps.debouncedValues)
+    );
+  }
+);
+
 const SignupComponent: React.FC<SignupComponentProps> = ({
   getter,
   setter,
@@ -24,14 +151,6 @@ const SignupComponent: React.FC<SignupComponentProps> = ({
   onInfo,
 }) => {
   const num = useMemo(() => Math.floor(Math.random() * 5) + 1, []);
-  const [checked, setChecked] = useState({
-    name: false,
-    clientId: false,
-    password: false,
-    passwordCheck: false,
-    phone: false,
-    email: false,
-  });
   const infoElement = useRef<HTMLImageElement>(null);
   const [inputValues, setInputValues] = useState({ ...getter, type: '' });
   const debouncedValues = useDebounce<SignupFields>(inputValues, 800);
@@ -45,52 +164,6 @@ const SignupComponent: React.FC<SignupComponentProps> = ({
       phone: debouncedValues.phone,
       email: debouncedValues.email,
     });
-
-    const type = debouncedValues.type;
-    const value = debouncedValues[type];
-
-    let result = false;
-    let pwCheck = checked.passwordCheck;
-
-    switch (type) {
-      case 'name':
-        result = nameValidationCheck(value);
-        break;
-      case 'clientId':
-        result = idValidationCheck(value);
-        break;
-      case 'password':
-        result = pwValidationCheck(value);
-        if (pwEqualValidationCheck(value, debouncedValues.passwordCheck))
-          pwCheck = true;
-        else pwCheck = false;
-        break;
-      case 'phone':
-        result = phoneValidationCheck(value);
-        break;
-      case 'email':
-        result = emailValidationCheck(value);
-        break;
-      default:
-        result = pwEqualValidationCheck(
-          debouncedValues.password,
-          debouncedValues.passwordCheck
-        );
-        break;
-    }
-
-    if (type === 'passwordCheck') {
-      setChecked({
-        ...checked,
-        ['passwordCheck']: result,
-      });
-    } else {
-      setChecked({
-        ...checked,
-        [type]: result,
-        ['passwordCheck']: pwCheck,
-      });
-    }
   }, [debouncedValues, setter]);
 
   useEffect(() => {
@@ -134,65 +207,9 @@ const SignupComponent: React.FC<SignupComponentProps> = ({
           </p>
         </div>
         <S.SignupInputBox>
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => onChange(e)}
-            placeholder="리그오브레전드 닉네임을 입력하세요."
-            style={{
-              borderBottom: checked.name ? '2px solid green' : '2px solid red',
-            }}
-          />
-          <input
-            type="text"
-            name="clientId"
-            onChange={(e) => onChange(e)}
-            placeholder="아이디를 입력하세요."
-            style={{
-              borderBottom: checked.clientId
-                ? '2px solid green'
-                : '2px solid red',
-            }}
-          />
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => onChange(e)}
-            placeholder="비밀번호를 입력하세요."
-            style={{
-              borderBottom: checked.password
-                ? '2px solid green'
-                : '2px solid red',
-            }}
-          />
-          <input
-            type="password"
-            name="passwordCheck"
-            onChange={(e) => onChange(e)}
-            placeholder="비밀번호를 다시 한번 입력하세요."
-            style={{
-              borderBottom: checked.passwordCheck
-                ? '2px solid green'
-                : '2px solid red',
-            }}
-          />
-          <input
-            type="text"
-            name="phone"
-            onChange={(e) => onChange(e)}
-            placeholder="전화번호를 입력하세요."
-            style={{
-              borderBottom: checked.phone ? '2px solid green' : '2px solid red',
-            }}
-          />
-          <input
-            type="text"
-            name="email"
-            onChange={(e) => onChange(e)}
-            placeholder="이메일을 입력하세요."
-            style={{
-              borderBottom: checked.email ? '2px solid green' : '2px solid red',
-            }}
+          <InputFieldsForm
+            debouncedValues={debouncedValues}
+            onChange={onChange}
           />
           <S.SignupBtnBox>
             <button onClick={onSignup}>회원가입</button>
