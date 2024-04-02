4ab7b834fe0842045cb8cb9c05ea73dba81c0930
유영 <dbdud1999@naver.com>
2024-04-02 11:54:42 +0900

    #9 :bulb: 소스코드 주석 추가

--- server/lbti/src/main/java/com/dkslgg/lbti/model/service/LbtiServiceImpl.java
+++ server/lbti/src/main/java/com/dkslgg/lbti/model/service/LbtiServiceImpl.java
@@ -24,9 +24,10 @@ public class LbtiServiceImpl implements LbtiService {
     @Override
     @Transactional(rollbackFor = LbtiException.class)
     public List<LbtiQuestionReadResponseDto> readLbtiQuestionList() {
+        // LBTI 문항 조회
         List<LbtiQuestion> lbtiQuestionList = lbtiQuestionRepository.findAll();
         List<LbtiQuestionReadResponseDto> lbtiQuestionDtoList = new ArrayList<>();
-        for (LbtiQuestion lbtiQuestion : lbtiQuestionList) {
+        for (LbtiQuestion lbtiQuestion : lbtiQuestionList) {  // Question 순회
             if (lbtiQuestion.getId() <= 0) {
                 log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Id 유효하지 않음");
                 throw new LbtiException(ErrorMessage.QUESTION_INVALID);
@@ -37,7 +38,7 @@ public class LbtiServiceImpl implements LbtiService {
             }
 
             List<LbtiAnswerReadResponseDto> lbtiAnswerDtoList = new ArrayList<>();
-            for (LbtiAnswer lbtiAnswer : lbtiQuestion.getLbtiAnswerList()) {
+            for (LbtiAnswer lbtiAnswer : lbtiQuestion.getLbtiAnswerList()) {  // Answer 순회
                 if (lbtiQuestion.getContent() == null || lbtiQuestion.getContent().isBlank()) {
                     log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Answer Content 유효하지 않음");
                     throw new LbtiException(ErrorMessage.QUESTION_INVALID);
@@ -50,13 +51,13 @@ public class LbtiServiceImpl implements LbtiService {
                         .content(lbtiAnswer.getContent())
                         .itemCode(lbtiAnswer.getItemCode())
                         .score(lbtiAnswer.getScore())
-                        .build());
+                        .build());  // Answer 삽입
             }
 
             lbtiQuestionDtoList.add(LbtiQuestionReadResponseDto.builder()
                     .content(lbtiQuestion.getContent())
                     .answerList(lbtiAnswerDtoList)
-                    .build());
+                    .build());  // Question 삽입
         }
 
         return lbtiQuestionDtoList;
