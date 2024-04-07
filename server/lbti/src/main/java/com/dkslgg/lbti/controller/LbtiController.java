dd716c42dabc310313b30748e5af24510f94eb8a
유영 <dbdud1999@naver.com>
2024-04-07 22:17:58 +0900

    #9 :sparkles: LBTI 결과 조회 API 추가

--- server/lbti/src/main/java/com/dkslgg/lbti/controller/LbtiController.java
+++ server/lbti/src/main/java/com/dkslgg/lbti/controller/LbtiController.java
@@ -1,19 +1,32 @@
 package com.dkslgg.lbti.controller;
 
 import com.dkslgg.lbti.model.service.LbtiService;
+import com.dkslgg.lbti.util.ErrorMessage;
+import com.dkslgg.lbti.util.LbtiException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lbti")
 @RequiredArgsConstructor
+@Slf4j
 public class LbtiController {
     private final LbtiService lbtiService;
     @GetMapping("question")
     public ResponseEntity<?> readLbtiQuestionList() {
         return ResponseEntity.ok(lbtiService.readLbtiQuestionList());
+    }
+    @GetMapping("{lbtiResult}")
+    public ResponseEntity<?> readLbtiResult(@PathVariable String lbtiResult) {
+        if(lbtiResult == null || lbtiResult.length() != 4) {
+            log.error("LbtiController.readLbtiResult() LBTI 결과 유효하지 않음");
+            throw new LbtiException(ErrorMessage.RESULT_NOT_FOUND);
+        }
+        return ResponseEntity.ok(lbtiService.readLbtiResult(lbtiResult));
     }
 }
