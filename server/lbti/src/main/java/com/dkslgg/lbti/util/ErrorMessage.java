7bbcf2a4d5ccc184bd294425b3bc0d1b2c2246e6
유영 <dbdud1999@naver.com>
2024-04-07 22:28:40 +0900

    #9 :art: 에러 코드 수정

--- server/lbti/src/main/java/com/dkslgg/lbti/util/ErrorMessage.java
+++ server/lbti/src/main/java/com/dkslgg/lbti/util/ErrorMessage.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    RESULT_NOT_FOUND(INTERNAL_SERVER_ERROR, "LBTI 결과를 찾을 수 없습니다."),
+    RESULT_NOT_FOUND(BAD_REQUEST, "LBTI 결과를 찾을 수 없습니다."),
     QUESTION_INVALID(INTERNAL_SERVER_ERROR, "LBTI 문항이 유효하지 않습니다."),
     RESULT_INVALID(INTERNAL_SERVER_ERROR, "LBTI 결과가 유효하지 않습니다.");
 
