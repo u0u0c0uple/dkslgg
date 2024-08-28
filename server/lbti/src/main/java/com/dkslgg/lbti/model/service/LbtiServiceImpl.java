fad0c84a81d6da480e1bce88744f1de3c7ecfaa2
유영 <dbdud1999@naver.com>
2024-08-28 13:32:34 +0900

    #14 :art: Exception 분기 합치기

--- server/lbti/src/main/java/com/dkslgg/lbti/model/service/LbtiServiceImpl.java
+++ server/lbti/src/main/java/com/dkslgg/lbti/model/service/LbtiServiceImpl.java
@@ -1,8 +1,8 @@
 package com.dkslgg.lbti.model.service;
 
-import com.dkslgg.lbti.model.dto.response.LbtiAnswerReadResponseDto;
-import com.dkslgg.lbti.model.dto.response.LbtiQuestionReadResponseDto;
-import com.dkslgg.lbti.model.dto.response.LbtiResultReadResponseDto;
+import com.dkslgg.lbti.model.dto.response.LbtiAnswerResponseDto;
+import com.dkslgg.lbti.model.dto.response.LbtiQuestionResponseDto;
+import com.dkslgg.lbti.model.dto.response.LbtiResultResponseDto;
 import com.dkslgg.lbti.model.entity.LbtiAnswer;
 import com.dkslgg.lbti.model.entity.LbtiQuestion;
 import com.dkslgg.lbti.model.entity.LbtiResult;
@@ -25,40 +25,35 @@ public class LbtiServiceImpl implements LbtiService {
     private final LbtiQuestionRepository lbtiQuestionRepository;
     private final LbtiResultRepository lbtiResultRepository;
 
+    /**
+     * LBTI 문항 리스트를 조회하는 메소드
+     */
     @Override
     @Transactional(rollbackFor = LbtiException.class)
-    public List<LbtiQuestionReadResponseDto> readLbtiQuestionList() {
+    public List<LbtiQuestionResponseDto> readLbtiQuestionList() {
         // LBTI 문항 조회
         List<LbtiQuestion> lbtiQuestionList = lbtiQuestionRepository.findAll();
-        List<LbtiQuestionReadResponseDto> lbtiQuestionDtoList = new ArrayList<>();
+        List<LbtiQuestionResponseDto> lbtiQuestionDtoList = new ArrayList<>();
         for (LbtiQuestion lbtiQuestion : lbtiQuestionList) {  // Question 순회
-            if (lbtiQuestion.getId() <= 0) {
-                log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Id 유효하지 않음");
-                throw new LbtiException(ErrorMessage.QUESTION_INVALID);
-            }
-            if (lbtiQuestion.getContent() == null || lbtiQuestion.getContent().isBlank()) {
-                log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Content 유효하지 않음");
+            if (lbtiQuestion.getId() <= 0 || lbtiQuestion.getContent() == null || lbtiQuestion.getContent().isBlank()) {
+                log.error("Lbti 문항 ID 혹은 내용이 유효하지 않음 : LbtiQuestion\\{id={}, content={}\\}", lbtiQuestion.getId(), lbtiQuestion.getContent());
                 throw new LbtiException(ErrorMessage.QUESTION_INVALID);
             }
 
-            List<LbtiAnswerReadResponseDto> lbtiAnswerDtoList = new ArrayList<>();
-            for (LbtiAnswer lbtiAnswer : lbtiQuestion.getLbtiAnswerList()) {  // Answer 순회
-                if (lbtiQuestion.getContent() == null || lbtiQuestion.getContent().isBlank()) {
-                    log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Answer Content 유효하지 않음");
+            List<LbtiAnswerResponseDto> lbtiAnswerDtoList = new ArrayList<>();
+            for (LbtiAnswer lbtiAnswer : lbtiQuestion.getLbtiAnswerList()) {  // 각 문항에 대한 Answer 순회
+                if (lbtiAnswer.getContent() == null || lbtiAnswer.getContent().isBlank() || lbtiAnswer.getScore() <= 0) {
+                    log.error("Lbti 문항 응답 유효하지 않음 : LbtiAnswer\\{content={}, score={}\\}", lbtiAnswer.getContent(), lbtiAnswer.getScore());
                     throw new LbtiException(ErrorMessage.QUESTION_INVALID);
                 }
-                if (lbtiAnswer.getScore() <= 0) {
-                    log.error("LbtiServiceImpl.readLbtiQuestionByQuestionId() Answer Score 유효하지 않음.");
-                    throw new LbtiException(ErrorMessage.QUESTION_INVALID);
-                }
-                lbtiAnswerDtoList.add(LbtiAnswerReadResponseDto.builder()
+                lbtiAnswerDtoList.add(LbtiAnswerResponseDto.builder()
                         .content(lbtiAnswer.getContent())
                         .itemCode(lbtiAnswer.getItemCode())
                         .score(lbtiAnswer.getScore())
                         .build());  // Answer 삽입
             }
 
-            lbtiQuestionDtoList.add(LbtiQuestionReadResponseDto.builder()
+            lbtiQuestionDtoList.add(LbtiQuestionResponseDto.builder()
                     .content(lbtiQuestion.getContent())
                     .answerList(lbtiAnswerDtoList)
                     .build());  // Question 삽입
@@ -67,14 +62,17 @@ public class LbtiServiceImpl implements LbtiService {
         return lbtiQuestionDtoList;
     }
 
+    /**
+     * LBTI 응답에 따른 결과를 반환하는 메소드
+     */
     @Override
-    public LbtiResultReadResponseDto readLbtiResult(String lbtiResultStr) {
+    public LbtiResultResponseDto readLbtiResult(String lbtiResultStr) {
         LbtiResult lbtiResult = lbtiResultRepository.findById(lbtiResultStr).orElseThrow(() -> {
-            log.error("LbtiServiceImpl.readLbtiResult() LBTI 문자열 유효하지 않음");
-            throw new LbtiException(ErrorMessage.RESULT_NOT_FOUND);
+            log.error("요청받은 LBTI 문자열 유효하지 않음 : {}", lbtiResultStr);
+            return new LbtiException(ErrorMessage.RESULT_NOT_FOUND);
         });
 
-        return LbtiResultReadResponseDto.builder()
+        return LbtiResultResponseDto.builder()
                 .code(lbtiResult.getCode())
                 .title(lbtiResult.getTitle())
                 .content(lbtiResult.getContent())
