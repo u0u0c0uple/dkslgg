67b205bd44354a86dc0abc27bf748f5ef2c805da
유영 <dbdud1999@naver.com>
2024-08-28 13:29:50 +0900

    #14 :bug: Cross Origin 추가로 CORS 방지

--- server/lbti/src/main/java/com/dkslgg/lbti/controller/LbtiController.java
+++ server/lbti/src/main/java/com/dkslgg/lbti/controller/LbtiController.java
@@ -6,24 +6,24 @@ import com.dkslgg.lbti.util.LbtiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/lbti")
 @RequiredArgsConstructor
+@CrossOrigin
 @Slf4j
 public class LbtiController {
     private final LbtiService lbtiService;
+
     @GetMapping("question")
     public ResponseEntity<?> readLbtiQuestionList() {
         return ResponseEntity.ok(lbtiService.readLbtiQuestionList());
     }
+
     @GetMapping("{lbtiResult}")
     public ResponseEntity<?> readLbtiResult(@PathVariable String lbtiResult) {
-        if(lbtiResult == null || lbtiResult.length() != 4) {
+        if (lbtiResult == null || lbtiResult.length() != 4) {
             log.error("LbtiController.readLbtiResult() LBTI 결과 유효하지 않음");
             throw new LbtiException(ErrorMessage.RESULT_NOT_FOUND);
         }
